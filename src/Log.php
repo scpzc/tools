03c261d77a11f7a73b82160dd3ae901aa10f75f3
scpzc <335760480@qq.com>
2020-05-07 20:29:22 +0800

    工具包

--- src/Log.php
+++ src/Log.php
@@ -33,10 +33,7 @@ class Log
         $logContent = '【DATE:' . date("Y-m-d H:i:s") . '---';
         $logContent .= 'FILE:' . $backtrace[0]['file'] . '---';
         $logContent .= 'LINE:' . $backtrace[0]['line'] . '---';
-        $logContent .= 'PARAMS:POST:' . json_encode($_POST, JSON_UNESCAPED_UNICODE) . '---';
-        $logContent .= 'GET:' . json_encode($_GET, JSON_UNESCAPED_UNICODE) . '---';
-        $logContent .= 'PARAMS:' . json_encode($GLOBALS['params']??[], JSON_UNESCAPED_UNICODE).'---';
-        $logContent .= 'STREAM:' . file_get_contents('php://input') . '---';
+        $logContent .= 'PARAMS:' . json_encode(request()->all(), JSON_UNESCAPED_UNICODE) . '---';
         $logContent .= 'TYPE:' . $type . '---';
         $logContent .= 'MESSAGE:' . $content . '】';
         \Illuminate\Support\Facades\Log::info($logContent);
